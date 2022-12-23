7305f678051c26c7c54bdecd80b2739283fec690
roqurota <runandback@gmail.com>
2022-12-23 11:36:27 +0300

    last

--- src/Experience/Expreience.js
+++ src/Experience/Expreience.js
@@ -4,6 +4,9 @@ import Time from './Utils/Time.js';
 import Camera from './Camera.js'
 import Renderer from './Renderer.js';
 import World from './World/World.js';
+import Resources from './Utils/Resources.js';
+import Debug from './Utils/Debug.js';
+import sources from './sources.js'
 
 let instance = null;
 
@@ -22,9 +25,11 @@ export default class Experience {
         this.canvas = canvas
 
         // Setup
+        this.debug = new Debug()
         this.sizes = new Sizes();
         this.time = new Time();
         this.scene = new THREE.Scene();
+        this.resources = new Resources(sources)
         this.camera = new Camera();
         this.renderer = new Renderer();
         this.world = new World()
@@ -40,11 +45,36 @@ export default class Experience {
 
     update() {
         this.camera.update()
+        this.world.update()
         this.renderer.update()
     }
 
     resize() {
         this.camera.resize()
         this.renderer.resize()
+    }
+
+    destroy() {
+        this.sizes.off('resize')
+        this.time.off('tick')
+
+        this.scene.traverse(child => {
+            if (child instanceof THREE.Mesh) {
+                child.geometry.dispose()
+
+                for (const key in child.material) {
+                    const value = child.material[key]
+
+                    if (value && typeof value.dispose === 'function')
+                        value.dispose()
+                }
+            }
+        })
+
+        this.camera.controls.dispose()
+        this.renderer.instance.dispose()
+
+        if (this.debug.active)
+            this.debug.ui.destroy()
     }
 }
--- src/Experience/World/World.js
+++ src/Experience/World/World.js
@@ -1,16 +1,25 @@
 import * as THREE from 'three'
 import Experience from "../Expreience";
+import Environment from './Environment';
+import Floor from './Floor.js';
+import Fox from './Fox.js';
 
 export default class World {
     constructor() {
         this.experience = new Experience()
         this.scene = this.experience.scene
-        
-        // Test Mesh
-        const testMesh = new THREE.Mesh(
-            new THREE.BoxGeometry(1, 1, 1),
-            new THREE.MeshBasicMaterial({color: 'red', wireframe: true})
-        )
-        this.scene.add(testMesh)
+        this.resources = this.experience.resources
+
+        // Wait for resources
+        this.resources.on('ready', () => {
+            this.floor = new Floor()
+            this.fox = new Fox()
+            this.environment = new Environment()
+        })
+    }
+
+    update() {
+        if (this.fox)
+            this.fox.update()
     }
 }
